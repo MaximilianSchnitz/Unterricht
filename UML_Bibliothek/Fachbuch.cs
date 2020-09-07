b6213e212c93b321722239dfc0cb7091aa4d7f40
Felix Wagner <felixwa02@gmail.com>
2020-09-07 14:42:54 +0200

    Added UML_Bibliothek

---
+++ UML_Bibliothek/Fachbuch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UML_Bibliothek
+{
+    class Fachbuch : Druckmedium
+    {
+
+        private string fachbereich;
+        private string themenfeld;
+
+        public Fachbuch(string titel, DateTime erscheinungsjahr, string isbn, string author, int seitenzahl, string fachbereich, string themenfeld)
+            : base(titel, erscheinungsjahr, isbn, author, seitenzahl)
+        {
+            this.fachbereich = fachbereich;
+            this.themenfeld = themenfeld;
+        }
+
+        public string PruefeAktualitaet()
+        {
+            int diff = (int)(DateTime.Now - Erscheinungsjahr).TotalDays / 365;
+            if (diff > 10)
+                return "nicht aktuell";
+            else if (diff >= 2)
+                return "aktuell";
+            else if (diff >= 0)
+                return "sehr aktuell";
+            else
+                return "Es ist kaputt gegangen";
+        }
+
+    }
+}
---
+++ UML_Bibliothek/Film.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UML_Bibliothek
+{
+    class Film : Medium
+    {
+
+        private string regisseur;
+        private int laufzeit;
+        private int altersfreigabe;
+
+        public Film(string titel, DateTime erscheinungsjahr, string regisseur, int laufzeit, int altersfreigabe)
+            : base(titel, erscheinungsjahr)
+        {
+            this.regisseur = regisseur;
+            this.laufzeit = laufzeit;
+            this.altersfreigabe = altersfreigabe;
+        }
+
+        public bool PruefeAltersfreigabe(Ausleiher ausleiher)
+        {
+            return (DateTime.Now - ausleiher.Geburtsdatum).TotalDays / 365 >= altersfreigabe;
+        }
+
+        public override void Ausleihen(Ausleiher ausleiher)
+        {
+            if (PruefeAltersfreigabe(ausleiher))
+                base.Ausleihen(ausleiher);
+            else
+                Console.WriteLine($"{ausleiher.Vorname} {ausleiher.Name} ist zu jung für {Titel}");
+        }
+
+    }
+}
